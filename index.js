906515564b570732d83be179ea685006621f127f
Ken Takahashi <takahashi.ken.716@gmail.com>
2020-03-25 08:14:25 +0700

    created test for getTodos api.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const express = require("express");
-const todosRouter = require("./routers/todos.js");
-const app = express();
+const app = require("./app.js");
 const PORT = 8080;
-
-app.use("/api/todos", todosRouter);
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT} `);
